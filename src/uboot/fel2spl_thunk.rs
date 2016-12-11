e39e5b2fe50b6d479f2beb54eccee1b2e711b45d
Razican <razican@protonmail.ch>
2016-12-11 20:50:08 +0100

    Added SPL/U-Boot loading

---
+++ src/uboot/fel2spl_thunk.rs
@@ -0,0 +1,132 @@
+pub const FEL_TO_SPL_THUNK: [u32; 66] = [//   0:    b          5c <setup_stack>
+                                         0x_ea_00_00_15,
+                                         //   4:    nop
+                                         0x_e1_a0_00_00,
+                                         //   8:    nop
+                                         0x_e1_a0_00_00,
+                                         //   c:    nop
+                                         0x_e1_a0_00_00,
+                                         //  10:    nop
+                                         0x_e1_a0_00_00,
+                                         //  14:    nop
+                                         0x_e1_a0_00_00,
+                                         //  18:    nop
+                                         0x_e1_a0_00_00,
+                                         //  1c:    nop
+                                         0x_e1_a0_00_00,
+                                         //  20:    nop
+                                         0x_e1_a0_00_00,
+                                         //  24:    nop
+                                         0x_e1_a0_00_00,
+                                         //  28:    add        r4, pc, #220
+                                         0x_e2_8f_40_dc,
+                                         //  2c:    ldr        r0, [r4], #4
+                                         0x_e4_94_00_04,
+                                         //  30:    ldr        r1, [r4], #4
+                                         0x_e4_94_10_04,
+                                         //  34:    ldr        r6, [r4], #4
+                                         0x_e4_94_60_04,
+                                         //  38:    cmp        r6, #0
+                                         0x_e3_56_00_00,
+                                         //  3c:    bxeq       lr
+                                         0x_01_2f_ff_1e,
+                                         //  40:    ldr        r2, [r0]
+                                         0x_e5_90_20_00,
+                                         //  44:    ldr        r3, [r1]
+                                         0x_e5_91_30_00,
+                                         //  48:    subs       r6, r6, #4
+                                         0x_e2_56_60_04,
+                                         //  4c:    str        r2, [r1], #4
+                                         0x_e4_81_20_04,
+                                         //  50:    str        r3, [r0], #4
+                                         0x_e4_80_30_04,
+                                         //  54:    bne        40 <swap_next_word>
+                                         0x_1a_ff_ff_f9,
+                                         //  58:    b          2c <swap_next_buffer>
+                                         0x_ea_ff_ff_f3,
+                                         //  5c:    ldr        r8, [pc, #164]
+                                         0x_e5_9f_80_a4,
+                                         //  60:    sub        r0, pc, #68
+                                         0x_e2_4f_00_44,
+                                         //  64:    str        sp, [r0, #-4]!
+                                         0x_e5_20_d0_04,
+                                         //  68:    mov        sp, r0
+                                         0x_e1_a0_d0_00,
+                                         //  6c:    mrs        r2, CPSR
+                                         0x_e1_0f_20_00,
+                                         //  70:    push       {r2, lr}
+                                         0x_e9_2d_40_04,
+                                         //  74:    orr        r2, r2, #192
+                                         0x_e3_82_20_c0,
+                                         //  78:    msr        CPSR_c, r2
+                                         0x_e1_21_f0_02,
+                                         //  7c:    mrc        15, 0, r2, cr1, cr0, {0}
+                                         0x_ee_11_2f_10,
+                                         //  80:    movw       r3, #4100
+                                         0x_e3_01_30_04,
+                                         //  84:    tst        r2, r3
+                                         0x_e1_12_00_03,
+                                         //  88:    bne        d8 <cache_is_unsupported>
+                                         0x_1a_00_00_12,
+                                         //  8c:    bl         28 <swap_all_buffers>
+                                         0x_eb_ff_ff_e5,
+                                         //  90:    movw       r7, #27705
+                                         0x_e3_06_7c_39,
+                                         //  94:    movt       r7, #24330
+                                         0x_e3_45_7f_0a,
+                                         //  98:    mov        r0, r8
+                                         0x_e1_a0_00_08,
+                                         //  9c:    ldr        r5, [r0, #16]
+                                         0x_e5_90_50_10,
+                                         //  a0:    ldr        r2, [r0], #4
+                                         0x_e4_90_20_04,
+                                         //  a4:    subs       r5, r5, #4
+                                         0x_e2_55_50_04,
+                                         //  a8:    add        r7, r7, r2
+                                         0x_e0_87_70_02,
+                                         //  ac:    bne        a0 <check_next_word>
+                                         0x_1a_ff_ff_fb,
+                                         //  b0:    ldr        r2, [r8, #12]
+                                         0x_e5_98_20_0c,
+                                         //  b4:    subs       r7, r7, r2, lsl #1
+                                         0x_e0_57_70_82,
+                                         //  b8:    bne        e8 <checksum_is_bad>
+                                         0x_1a_00_00_0a,
+                                         //  bc:    movw       r2, #17966
+                                         0x_e3_04_26_2e,
+                                         //  c0:    movt       r2, #19525
+                                         0x_e3_44_2c_45,
+                                         //  c4:    str        r2, [r8, #8]
+                                         0x_e5_88_20_08,
+                                         //  c8:    dsb        sy
+                                         0x_f5_7f_f0_4f,
+                                         //  cc:    isb        sy
+                                         0x_f5_7f_f0_6f,
+                                         //  d0:    blx        r8
+                                         0x_e1_2f_ff_38,
+                                         //  d4:    b          f4 <return_to_fel>
+                                         0x_ea_00_00_06,
+                                         //  d8:    movw       r2, #16174
+                                         0x_e3_03_2f_2e,
+                                         //  dc:    movt       r2, #16191
+                                         0x_e3_43_2f_3f,
+                                         //  e0:    str        r2, [r8, #8]
+                                         0x_e5_88_20_08,
+                                         //  e4:    b          f8 <return_to_fel_noswap>
+                                         0x_ea_00_00_03,
+                                         //  e8:    movw       r2, #16942
+                                         0x_e3_04_22_2e,
+                                         //  ec:    movt       r2, #17473
+                                         0x_e3_44_24_41,
+                                         //  f0:    str        r2, [r8, #8]
+                                         0x_e5_88_20_08,
+                                         //  f4:    bl         28 <swap_all_buffers>
+                                         0x_eb_ff_ff_cb,
+                                         //  f8:    pop        {r2, lr}
+                                         0x_e8_bd_40_04,
+                                         //  fc:    msr        CPSR_c, r2
+                                         0x_e1_21_f0_02,
+                                         // 100:    ldr        sp, [sp]
+                                         0x_e5_9d_d0_00,
+                                         // 104:    bx         lr
+                                         0x_e1_2f_ff_1e];
---
+++ src/uboot/mod.rs
@@ -0,0 +1,264 @@
+use std::{fmt, thread};
+use std::time::Duration;
+
+use byteorder::{LittleEndian, BigEndian, ByteOrder};
+
+mod fel2spl_thunk;
+use self::fel2spl_thunk::*;
+
+use ::error::*;
+use super::{FelHandle, u32_as_u8, SPL_LEN_LIMIT};
+
+/// *U-boot* image name length.
+const UBOOT_IH_NMLEN: u32 = 32;
+/// *U-Boot* image magic number.
+const UBOOT_IH_MAGIC: u32 = 0x27051956;
+/// ARM architechture constant in *U-Boot*.
+const UBOOT_IH_ARCH_ARM: u8 = 0x02;
+/// Offset of name field.
+const UBOOT_HEADER_NAME_OFFSET: u32 = 32;
+/// Header of the *U-Boot* header.
+const UBOOT_HEADER_SIZE: u32 = (UBOOT_HEADER_NAME_OFFSET + UBOOT_IH_NMLEN);
+
+/// *U-boot* image type.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum UbootImageType {
+    /// Firmware image.
+    Firmware,
+    /// Script file.
+    Script,
+}
+
+impl UbootImageType {
+    fn from_byte(byte: u8) -> Result<UbootImageType> {
+        match byte {
+            0x05 => Ok(UbootImageType::Firmware),
+            0x06 => Ok(UbootImageType::Script),
+            t => bail!("Unknown image type ({:#04x})", t),
+        }
+    }
+}
+
+impl fmt::Display for UbootImageType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let text = match *self {
+            UbootImageType::Firmware => "firmware",
+            UbootImageType::Script => "script",
+        };
+        write!(f, "{}", text)
+    }
+}
+
+impl<'h> FelHandle<'h> {
+    /// This function tests a given buffer for a valid *U-Boot* image. Upon success, the image data
+    /// gets transferred to the default memory address stored within the image header; and the
+    /// function returns the *U-Boot* entry point (offset) and size values, in that order.
+    pub fn write_uboot_image(&self, uboot: &[u8]) -> Result<(u32, u32)> {
+        if uboot.len() <= UBOOT_HEADER_SIZE as usize {
+            bail!("insufficient U-boot size");
+        }
+
+        let image_type = get_image_type(uboot)?;
+        if image_type != UbootImageType::Firmware {
+            bail!("expected firmware image, got {} image", image_type)
+        }
+
+        let data_size = BigEndian::read_u32(&uboot[12..16]);
+        let load_addr = BigEndian::read_u32(&uboot[16..20]);
+        if data_size != uboot.len() as u32 - UBOOT_HEADER_SIZE {
+            bail!("U-Boot image data size mismatch: expected {:#010x}, got {:#010x}",
+                  uboot.len() as u32 - UBOOT_HEADER_SIZE,
+                  data_size);
+        }
+        // TODO (from sunxi-fel): Verify image data integrity using the checksum field `ih_dcrc`,
+        // available from `BigEndian::read_u32(&uboot[24..28])`.
+        //
+        // However, this requires *CRC* routines that mimic their *U-Boot* counterparts, namely
+        // `image_check_dcrc()` in `${U-BOOT}/common/image.c` and `crc_wd()` in
+        // `${U-BOOT}/lib/crc32.c`.
+
+        // If we get here, we're "good to go" (i.e. actually write the data)
+        self.fel_write(load_addr, &uboot[UBOOT_HEADER_SIZE as usize..])
+            .chain_err(|| "could not write the U-Boot image to the device")?;
+
+        Ok((load_addr, data_size))
+    }
+
+    /// Writes *U-Boot* *SPL* into memory and executes it.
+    pub fn write_and_execute_spl(&self, spl: &[u8]) -> Result<()> {
+        assert!(spl.len() >= 32, "SPL length must be bigger than 32 bytes");
+
+        let mut spl_checksum = 2 * LittleEndian::read_u32(&spl[12..16]) - 0x5F0A6C39;
+        let spl_len = LittleEndian::read_u32(&spl[16..20]);
+
+        if spl_len > spl.len() as u32 || (spl_len % 4) != 0 {
+            return Err(Error::from_kind(ErrorKind::SPLHeader("bad length in the provided SPL \
+                                                              eGON header")));
+        }
+
+        for i in spl.chunks(4) {
+            spl_checksum = spl_checksum.wrapping_sub(LittleEndian::read_u32(i));
+        }
+        if spl_checksum != 0 {
+            return Err(Error::from_kind(ErrorKind::SPLHeader("the given SPL checksum does not \
+                                                              match")));
+        }
+
+        if self.soc_info.needs_l2en() {
+            self.enable_l2_cache()
+                .chain_err(|| "the SoC requires L2 cache but it couldn't be enabled")?;
+        }
+
+        let (sp_irq, sp) = self.get_stack_info()
+            .chain_err(|| "could not retrieve stack information")?;
+
+        let tt = match self.backup_and_disable_mmu()
+            .chain_err(|| "could not backup and disable the MMU translation table")? {
+            Some(tt) => Some(tt),
+            None => {
+                if let Some(mmu_tt_addr) = self.soc_info.get_mmu_tt_addr() {
+                    if (mmu_tt_addr & 0x00003FFF) != 0 {
+                        bail!("The MMU translation table address was not 16kB aligned");
+                    }
+
+                    // These settings are used by the *BROM* in A10/A13/A20 and we replicate them
+                    // here when enabling the *MMU*. The `DACR` value `0x55555555` means that
+                    // accesses are checked against the permission bits in the translation tables
+                    // for all domains. The `TTBCR` value `0x00000000` means that the short
+                    // descriptor translation table format is used, `TTBR0` is used for all the
+                    // possible virtual addresses (`N=0`) and that the translation table must be
+                    // aligned at a *16kB* boundary.
+                    self.set_dacr(0x55555555);
+                    self.set_ttbcr(0x00000000);
+                    self.set_ttbr0(mmu_tt_addr);
+
+                    Some(FelHandle::default_mmu_translation_table())
+                } else {
+                    None
+                }
+            }
+        };
+
+        let mut cur_addr = self.soc_info.get_spl_addr();
+        let mut spl_len_limit = SPL_LEN_LIMIT;
+        let mut written = 0;
+        let mut left = spl.len();
+        for swap_buffers in self.soc_info.get_swap_buffers() {
+            if swap_buffers.get_buf2() >= self.soc_info.get_spl_addr() &&
+               swap_buffers.get_buf2() < self.soc_info.get_spl_addr() + spl_len_limit {
+                spl_len_limit = swap_buffers.get_buf2() - self.soc_info.get_spl_addr()
+            }
+            if left > 0 && cur_addr < swap_buffers.get_buf1() {
+                let mut tmp = (swap_buffers.get_buf1() - cur_addr) as usize;
+                if tmp > left {
+                    tmp = left;
+                }
+                self.fel_write(cur_addr, &spl[written..tmp + written])
+                    .chain_err(|| {
+                        format!("could not write the file buffer to the current address ({:#010x})",
+                                cur_addr)
+                    })?;
+                cur_addr += tmp as u32;
+                written += tmp;
+                left -= tmp;
+            }
+            if left > 0 && cur_addr == swap_buffers.get_buf1() {
+                let mut tmp = swap_buffers.get_size() as usize;
+                if tmp > left {
+                    tmp = left;
+                }
+                self.fel_write(swap_buffers.get_buf2(), &spl[written..tmp + written])
+                    .chain_err(|| {
+                        format!("could not write the file buffer to the second buffer ({:#010x})",
+                                cur_addr)
+                    })?;
+                cur_addr += tmp as u32;
+                written += tmp;
+                left -= tmp;
+            }
+        }
+
+        // Clarify the SPL size limitations, and bail out if they are not met
+        if self.soc_info.get_thunk_addr() < spl_len_limit {
+            spl_len_limit = self.soc_info.get_thunk_addr();
+        }
+
+        if spl_len > spl_len_limit {
+            bail!("SPL too large, size limit is {} bytes, but it has {} bytes",
+                  spl_len_limit,
+                  spl_len);
+        }
+
+        // Write the remaining part of the SPL
+        if left > 0 {
+            self.fel_write(cur_addr, &spl[written..left + written])
+                .chain_err(|| {
+                    format!("could not write the file buffer to the current address ({:#010x})",
+                            cur_addr)
+                })?;
+        }
+
+        let thunk_size = FEL_TO_SPL_THUNK.len() as u32 * 4 + 4 +
+                         (self.soc_info.get_swap_buffers().len() as u32 + 1) * 12;
+        if thunk_size > self.soc_info.get_thunk_size() {
+            bail!("bad thunk size, need {} bytes, but only {} bytes available",
+                  thunk_size,
+                  self.soc_info.get_thunk_size());
+        }
+
+        // Generate thunk buffer.
+        let mut thunk_buf = Vec::with_capacity((thunk_size / 4) as usize);
+        thunk_buf.extend_from_slice(&FEL_TO_SPL_THUNK);
+        thunk_buf.push(self.soc_info.get_spl_addr());
+        for swap_buffers in self.soc_info.get_swap_buffers() {
+            thunk_buf.push(swap_buffers.get_buf1());
+            thunk_buf.push(swap_buffers.get_buf2());
+            thunk_buf.push(swap_buffers.get_size());
+        }
+        if cfg!(not(target_endian = "little")) {
+            for word in &mut thunk_buf {
+                *word = word.to_le();
+            }
+        }
+        thunk_buf.extend_from_slice(&[0u32; 3]); // Empty buffer size
+
+        // Execute SPL.
+        self.fel_write(self.soc_info.get_thunk_addr(), u32_as_u8(&thunk_buf))
+            .chain_err(|| "could not write thunk buffer to memory")?;
+        self.fel_execute(self.soc_info.get_thunk_addr())
+            .chain_err(|| "could not execute SPL code in thunk address")?;
+
+        // TODO Try to find and fix the bug, which needs this workaround
+        thread::sleep(Duration::from_millis(250));
+
+        // Read back the result and check if everything was fine
+        let mut signature = [0u8; 8];
+        self.fel_read(self.soc_info.get_spl_addr() + 4, &mut signature)
+            .chain_err(|| "could not read SPL signature")?;
+        if &signature != b"eGON.FEL" {
+            bail!(format!("unable to validate SPL load. Expected 'eGON.FEL' signature. Failure \
+                           code: {}",
+                          String::from_utf8_lossy(&signature)));
+        }
+
+        // Re-enable the MMU if it was enabled by BROM
+        if let Some(tt) = tt {
+            self.restore_and_enable_mmu(tt).chain_err(|| "unable to restore and enable MMU")?;
+        }
+        Ok(())
+    }
+}
+
+/// Utility function to determine the image type from a mkimage-compatible header at given
+/// buffer.
+fn get_image_type(image: &[u8]) -> Result<UbootImageType> {
+    debug_assert!(image.len() > UBOOT_HEADER_SIZE as usize,
+                  "insuficient image length");
+    if BigEndian::read_u32(&image[..4]) != UBOOT_IH_MAGIC {
+        bail!("U-Boot signature mismatch");
+    }
+    if image[29] != UBOOT_IH_ARCH_ARM {
+        bail!("invalid architechture in U-Boot header, only ARM is supported");
+    }
+    UbootImageType::from_byte(image[30])
+}
